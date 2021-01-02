218b12e9c4a47777aed595012ee3d3f4de46c115
chaquen <edgar.guzman21@gmail.com>
2021-01-01 21:55:05 -0500

    se quitan metodos sin uso creados automanticamente para controladores de tipo resource

--- public_html/viajemos.com/app/Http/Controllers/API/LibroController.php
+++ public_html/viajemos.com/app/Http/Controllers/API/LibroController.php
@@ -126,38 +126,5 @@ class LibroController extends Controller
             return Utilities::sendMessage(Utilities::COD_RESPONSE_HTTP_OK,$ex->getMessage(),true,Utilities::COD_RESPONSE_HTTP_OK);
         }
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    
 }
